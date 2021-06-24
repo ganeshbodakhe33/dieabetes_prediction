9d2590cd98135ba1ad11e03f5299a15902d6f87e
Jordan Sauchuk <jordansauchuk@Jordans-MacBook-Pro.local>
2021-06-24 14:31:27 -0400

    make it better

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask,request, url_for, redirect, render_template
 import pandas as pd
 import pickle
+from flask_cors import CORS
+app = Flask(__name__) 
+CORS(app)
 
-app = Flask(__name__)
 
 model = pickle.load(open("testing5.pkl", "rb"))
 
